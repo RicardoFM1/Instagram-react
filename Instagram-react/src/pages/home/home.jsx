505dfbfbd9bcf72d98445d72fe772f707de276f1
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-09-09 14:49:04 -0300

    feat(Stories agora são dinamicos por json)

--- Instagram-react/src/pages/home/home.jsx
+++ Instagram-react/src/pages/home/home.jsx
@@ -12,33 +12,72 @@ import BottomNavBar from "../../components/bottomNavBar/bottomNavBar";
 
 const Home = () => {
   const [salvo, setSalvo] = useState(false);
-
   const [posts, setPosts] = useState([
     {
       id: 1,
-      usuario: "liam_legal",
+      user: "liam_legal",
       likes: 100,
       comments: 265,
       reposts: 5,
       liked: false,
-      image:
+      profile:
         "https://plus.unsplash.com/premium_photo-1669741908308-5ca216f3fcd1?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       shares: 765,
       description: "Um cara bem legal fazendo algo legal",
     },
     {
       id: 2,
-      usuario: "liam_nao_legal",
+      user: "liam_nao_legal",
       likes: 1500,
       comments: 2000,
       reposts: 56,
       liked: false,
-      image:
-        "https://images.unsplash.com/photo-1626469028023-896eda77d814?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      profile:
+        "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       shares: 200,
       description: "Um cara não tão legal fazendo algo não tão legal",
     },
   ]);
+
+  const [stories, setStories] = useState([
+    {
+      id: 1,
+      user: 'Seu Story', // Nome vindo do backend normal, seria o próprio usuário logado, que com api verificaria o id com o id logado para formatar o texto
+      // para 'Seu story'
+      videos: [
+        // Colocar videos aqui em base64 ou um link.
+      ],
+      profile: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      // Foto de perfil dos stories
+    },
+    {
+      id: 2,
+      user: 'guizao_ds',
+      videos: [
+
+      ],
+      profile: "https://plus.unsplash.com/premium_photo-1689977807477-a579eda91fa2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 3,
+      user: 'duduardo_65',
+
+      videos: [
+
+      ],
+      profile: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 4,
+      user: '066_eduardo',
+      videos: [
+
+      ],
+      profile: "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1085&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+
+  ])
 
   const handleCurtido = (id) => {
     setPosts((estadoAnterior) =>
@@ -50,7 +89,7 @@ const Home = () => {
         return {
           ...post,
           likes: jaCurtido ? post.likes - 1 : post.likes + 1,
-          liked: !jaCurtido,
+          liked: !jaCurtido
         };
       }),
     );
@@ -58,7 +97,7 @@ const Home = () => {
 
   return (
     <>
-      <Stories />
+      <Stories stories={stories} setStories={setStories} />
       <main>
         <Stack className={style.listagemPosts}>
           {posts.map((post) => (
@@ -74,7 +113,7 @@ const Home = () => {
                     src="https://plus.unsplash.com/premium_photo-1695575578331-b09400a8a9bb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="Foto de perfil do post"
                   />
-                  <p className="mb-0 fw-semibold">{post.usuario}</p>
+                  <p className="mb-0 fw-semibold">{post.user}</p>
                 </section>
                 <Button className="border-0 bg-transparent text-black fw-bold">
                   ...
@@ -82,8 +121,8 @@ const Home = () => {
               </Stack>
               <img
                 className={style.imgPost}
-                src={post.image}
-                alt="Imagem post"
+                src={post.profile}
+                alt="profilem post"
               />
               <section>
                 <article className="d-flex p-3 pb-0 justify-content-between ">
@@ -131,7 +170,7 @@ const Home = () => {
                     <span className="fw-bold">outras 265 pessoas</span>
                   </p>
                   <p className="ps-3 mt-0">
-                    <span className="fw-bold">{post.usuario}</span>{" "}
+                    <span className="fw-bold">{post.user}</span>{" "}
                     {post.description}
                   </p>
                   <p className="ps-3 text-muted">
